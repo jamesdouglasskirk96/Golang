100c068d61399160d8ceb4bc24e927fd0ffc8166
mrjdk <39111267+jamesdouglasskirk96@users.noreply.github.com>
2018-05-12 14:20:00 -0500

    Add Project4Testing Dir

---
+++ Project4Testing/main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jamesdouglasskirk96/Golang/Project4Testing/variables"
+)
+
+func main() {
+	fmt.Println(variables.MyCharCount)
+}
---
+++ Project4Testing/variables/numbers.go
@@ -0,0 +1,3 @@
+package variables
+
+var MyCharCount = 10
