b206b76922fc296b16b58d8404bc00e20aa5c027
James Kirk <jameskirk@7senseslabs.com>
2018-07-07 19:37:38 -0500

    Adding Scan()

--- Project4Testing/main/main.go
+++ Project4Testing/main/main.go
@@ -1,31 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"github.com/jamesdouglasskirk96/Golang/Project4Testing/variables"
-	// "net/http"
-	// "io/ioutil"
-)
-
-func sum (x int) int {
-	return 10 + x
-}
+import "fmt"
 
 func main() {
-	fmt.Println(variables.MyCharCount)
-	fmt.Println(sum(24))
+	var name string
 
-	var x int = 1
-	increment := func () int {
-		x += 1
-		return x
-	}
-
-	fmt.Println(increment())
-	fmt.Println(increment())
-
-	// res, _ := http.Get("http://www.geekwiseacademy.com/")
-	// page, _ := ioutil.ReadAll(res.Body)
-	// res.Body.Close()
-	// fmt.Printf("%s", page)
+	fmt.Print("Please enter your name: ")
+	fmt.Scan(&name)
+	fmt.Println("Hello", name);
 }
